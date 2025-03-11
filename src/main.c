4e239a9cbd9384ccab171d565a23fad2c52b1d5d
Angele Barzilai <abarzila@z2r10p1.42lyon.fr>
2025-03-11 16:49:05 +0100

    fds OK

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: abarzila <abarzila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 08:48:06 by abarzila          #+#    #+#             */
-/*   Updated: 2025/03/11 14:57:21 by abarzila         ###   ########.fr       */
+/*   Updated: 2025/03/11 16:48:39 by abarzila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,15 @@
 #include <stdio.h>
 #include <sys/wait.h>
 
-
-
-
-static int	init_hyp_path(char **hyp_path, char **cmd_and_flags, char **path)
+static int	init_hyp_path(char **hyp_path, char **cmd_and_flags, char **env_path)
 {
 	char	*path_w_backslash;
 	int		i;
 
 	i = 0;
-	while (path[i])
+	while (env_path[i])
 	{
-		path_w_backslash = ft_strjoin(path[i], "/");
+		path_w_backslash = ft_strjoin(env_path[i], "/");
 		if (!path_w_backslash)
 		{
 			ft_putendl_fd("malloc failed", STDERR_FILENO);
@@ -81,39 +78,39 @@ static int	find_path(char **env)
 
 static char	*find_real_cmd(char **env, char **cmd_and_flags)
 {
-	char	**path;
+	char	**env_path;
 	char	**hypothetical_path_cmd;
 	char	*real_path;
 
-	path = ft_split(env[find_path(env)], ':');
-	if (!path)
+	env_path = ft_split(env[find_path(env)], ':');
+	if (!env_path)
 	{
 		ft_putendl_fd("malloc failed", STDERR_FILENO);
 		return (NULL);
 	}
-	path[0] = ft_strtrim_improved(path[0], "PATH=");
-	if (!path[0])
+	env_path[0] = ft_strtrim_improved(env_path[0], "PATH=");
+	if (!env_path[0])
 	{
-		free_tab(*path + 1);
+		free_tab(env_path);
 		ft_putendl_fd("malloc failed", STDERR_FILENO);
 		return (NULL);
 	}
-	hypothetical_path_cmd = malloc(sizeof (char *) * tablen(path));
+	hypothetical_path_cmd = malloc(sizeof (char *) * tablen(env_path));
 	if (!hypothetical_path_cmd)
 	{
-		free_tab(path);
+		free_tab(env_path);
 		ft_putendl_fd("malloc failed", STDERR_FILENO);
 		return (NULL);
 	}
-	if (init_hyp_path(hypothetical_path_cmd, cmd_and_flags, path) == 0)
+	if (init_hyp_path(hypothetical_path_cmd, cmd_and_flags, env_path) == 0)
 	{
 		// free_tab(*path + 1);
 		// free_tab(hypothetical_path_cmd);
 		// ft_putendl_fd("malloc failed", STDERR_FILENO);
 		return (NULL);
 	}
-	real_path = find_path_cmd(hypothetical_path_cmd, path);
-	free_tab(path);
+	real_path = find_path_cmd(hypothetical_path_cmd, env_path);
+	free_tab(env_path);
 	return (real_path);
 }
 
@@ -133,7 +130,7 @@ static int	manage_child(char *cmd, char **env)
 		ft_putendl_fd("malloc failed", STDERR_FILENO);
 		return (-1);
 	}
-	path_cmd = find_real_cmd(env, cmd_and_flags);
+	path_cmd = find_real_cmd(env, cmd_and_flags);/////////////////
 	if (!path_cmd)
 	{
 		return (-1);
@@ -148,6 +145,7 @@ static int	manage_child(char *cmd, char **env)
 		/*fail*/
 		return (-1);
 	}
+	return (0);
 }
 
 static void	manage_first_cmd(int *pipe_fd, char **av, char **env, pid_t *pid)
@@ -186,6 +184,8 @@ static void	manage_first_cmd(int *pipe_fd, char **av, char **env, pid_t *pid)
 			exit(EXIT_FAILURE);
 		}
 		close(pipe_fd[0]);
+		close(pipe_fd[1]);
+		close(infile);
 		if (manage_child(av[2], env) == -1)
 		{
 			close(infile);
@@ -216,7 +216,9 @@ static void	manage_last_cmd(int *pipe_fd, char **av, char **env, pid_t *pid)
 		{
 			/*fail*/
 		}
+		close(pipe_fd[0]);
 		close(pipe_fd[1]);
+		close(outfile);
 		manage_child(av[3], env);
 	}
 }
